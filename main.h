fa36c8984e770af195293df0cf7cc4c74d55c48d
TPriince <tobiloba110@gmail.com>
2022-04-18 19:03:33 -0700

    chore: add print_bases file

--- main.h
+++ main.h
@@ -16,7 +16,13 @@ int print_num(va_list args, int len);
 unsigned int find_length(unsigned int n, int base);
 int print_numbers(unsigned long n, unsigned int base, const char *digits);
 int putchar_int(int n, int len);
+
 int print_binary(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+int hex_check(int num, char x);
+
 char *rev_string(char *s);
 void write_base(char *str);
 unsigned int base_len(unsigned int num, int base);
