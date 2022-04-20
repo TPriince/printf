19de6dec23dec5036dfbe156c57d283f381dbb07
TPriince <tobiloba110@gmail.com>
2022-04-20 01:37:33 -0700

    feat: test new main functions

--- main.h
+++ main.h
@@ -1,46 +1,93 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
 /**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
+ * struct specifier - contains specifiers for printf
+ *
+ * @width: width of field to print
+ * @precision: precision of field to print
+ * @length: modifies type length. h/l flags. 0 default, +1 per l, -1 per h
+ * @widthflag: tells whether width is set
+ * @precisionflag: tells whether precision is set
+ * @left: left justify, '-'. 0 for right, 1 for left
+ * @sign: force sign, '+'. 0 for no, 1 for yes
+ * @space: space pad left side of positive numbers if leading 0s. 0 no 1 yes
+ * @zero: zero pad numbers up to width. 0 for no, 1 for yes.
+ * @zerox: add leading 0, 0x, or 0X to o, x or X specifiers. 0 no 1 yes
+ * @specifier: the data type to print, c, s, d, x, etc.
+ */
+typedef struct specifier
 {
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+	unsigned int width;
+	unsigned int precision;
+	int length;
+	char widthflag;
+	char precisionflag;
+	char left;
+	char sign;
+	char space;
+	char zero;
+	char zerox;
+	char specifier;
+} specifier;
 
-/*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
-int _printf(const char *format, ...);
-int _putchar(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
+/* from printf.c */
+int _printf(char *format, ...);
 
-/*Helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
+/* from prep_types.c */
+char *prep_string(char *str, specifier spec);
+char *prep_numeric(char *str, specifier spec);
+
+/* from utility.c */
+int _strlen(char *str);
+int ifputs(char *s);
+
+/* from itos.c */
+char *itos(va_list list);
+char *hitos(va_list list);
+char *litos(va_list list);
+char *hhitos(va_list list);
+
+/* from uitob.c */
+char *uitob(va_list list);
+char *luitob(va_list list);
+char *huitob(va_list list);
+char *hhuitob(va_list list);
+
+/* from utos.c */
+char *utos(va_list list);
+char *hutos(va_list list);
+char *hhutos(va_list list);
+char *lutos(va_list list);
+
+/* from itoo.c */
+char *itoo(va_list list);
+char *litoo(va_list list);
+char *hitoo(va_list list);
+char *hhitoo(va_list list);
+
+/* from itox.c */
+char *itox(va_list list);
+char *litox(va_list list);
+char *hitox(va_list list);
+char *hhitox(va_list list);
+
+/* from itox.c */
+char *itoX(va_list list);
+char *hitoX(va_list list);
+char *hhitoX(va_list list);
+char *litoX(va_list list);
+
+/* from stos.c */
+char *rev(char *str);
+char *rot(char *str);
+char *print_hidden(char *str);
+char *null(void);
+
+/* from ptos.c */
+char *nil(void);
 
 
 #endif
